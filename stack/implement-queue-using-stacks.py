b213493bdbc1674f77610ce8dc7725323b4990fd
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-10-08 02:16:56 -0700

    LC-implement-queue-using-stacks [Runtime: N/A; Memory: N/A]

---
+++ stack/implement-queue-using-stacks.py
@@ -0,0 +1,35 @@
+class MyQueue:
+
+    def __init__(self):
+        self.p = []
+        self.pp = []
+
+    def push(self, x: int) -> None:
+        self.p.append(x)
+        
+        
+    def pop(self) -> int:
+        # [1,2] -> [2,1]
+        if not self.pp and self.p:
+            while self.p:
+                self.pp.append(self.p.pop())
+        return self.pp.pop()
+
+
+
+    def peek(self) -> int:
+        if not self.pp and self.p:
+            while self.p:
+                self.pp.append(self.p.pop())
+        return self.pp[-1]
+
+    def empty(self) -> bool:
+        return not self.pp
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
