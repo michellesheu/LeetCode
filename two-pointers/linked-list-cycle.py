feeaaa51b525c5b70d3f7fa590401d19a1612381
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-10-07 23:47:02 -0700

    LC-linked-list-cycle [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/linked-list-cycle.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # 3 2 0 4 -> 2
+        #       s 
+        #       f 
+        # 3 2 0 4
+        #     s
+        #         f
+        slow = fast = head
+        while fast and slow.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                return True
+
+        return False
