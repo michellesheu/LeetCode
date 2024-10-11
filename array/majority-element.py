3203483285e5496c9459fb80c713b239ba01f1fc
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-10-10 17:37:25 -0700

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- array/majority-element.py
+++ array/majority-element.py
@@ -1,5 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        nums.sort()
-        return nums[n//2]
+        # Boyer Moore Voting algo
+        curr_candidate = nums[0]
+        count = 0
+        for i in range(1, len(nums)):
+            if curr_candidate == nums[i]:
+                count += 1
+            if count == 0:
+                curr_candidate = nums[i]
+        return curr_candidate
