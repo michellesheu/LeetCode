990a0d0fb86c94ccd0e58d22d180bb721f7eb833
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-10-10 23:59:30 -0700

    LC-add-binary [Runtime: 38 ms; Memory: 16.5 MB]

--- simulation/add-binary.py
+++ simulation/add-binary.py
@@ -1,3 +1,8 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return f"{int(a, 2) + int(b, 2):b}"
+        a, b = int(a,2), int(b,2)
+        while b: 
+            add_wo_carry = a ^ b 
+            carry = (a&b)<<1
+            a, b = add_wo_carry, carry 
+        return bin(a)[2:]
