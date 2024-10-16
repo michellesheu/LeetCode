5fe2423ff7ea5192cf1986097d9556d8ac0fd609
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-10-15 17:19:41 -0700

    LC-clone-graph [Runtime: 34 ms; Memory: 16.9 MB]

--- hash-table/clone-graph.py
+++ hash-table/clone-graph.py
@@ -5,23 +5,26 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
+from collections import deque
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        def dfs(node, visited):
-            if not node:
-                return node
-            
-            if node in visited:
-                return visited[node]
-            
-            clone = Node(node.val, [])
-            visited[node] = clone
-            
-            if node.neighbors:
-                clone.neighbors = [dfs(n, visited) for n in node.neighbors]
-            
-            return clone
-        
-        return dfs(node, {})
+        if not node:
+            return node
+        visited = {}
+        queue = deque([node])
+        clone = Node(node.val,[])
+        visited[node] = clone
+        while queue:
+            # Pop a node say "n" from the from the front of the queue.
+            n = queue.popleft()
+            # Iterate through all the neighbors of the node
+            for neighbor in n.neighbors:
+                if neighbor not in visited:
+                    # Clone the neighbor and put in the visited, if not present already
+                    visited[neighbor] = Node(neighbor.val, [])
+                    # Add the newly encountered node to the queue.
+                    queue.append(neighbor)
+                # Add the clone of the neighbor to the neighbors of the clone node "n".
+                visited[n].neighbors.append(visited[neighbor])
+        return visited[node]
