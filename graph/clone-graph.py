26c723ff93366f92ad543cefc1f93b2104f072e2
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-10-15 17:12:52 -0700

    LC-clone-graph [Runtime: 44 ms; Memory: 17.1 MB]

--- graph/clone-graph.py
+++ graph/clone-graph.py
@@ -8,17 +8,20 @@ class Node:
 
 from typing import Optional
 class Solution:
-    def __init__(self):
-        # Dictionary to save the visited node and it's respective clone
-        # as key and value respectively. This helps to avoid cycles.
-        self.visited = {}
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return node
-        if node in self.visited:
-            return self.visited[node]
-        clone = Node(node.val, [])
-        self.visited[node] = clone
-        if node.neighbors:
-            clone.neighbors = [self.cloneGraph(n) for n in node.neighbors]
-        return clone
+        def dfs(node, visited):
+            if not node:
+                return node
+            
+            if node in visited:
+                return visited[node]
+            
+            clone = Node(node.val, [])
+            visited[node] = clone
+            
+            if node.neighbors:
+                clone.neighbors = [dfs(n, visited) for n in node.neighbors]
+            
+            return clone
+        
+        return dfs(node, {})
