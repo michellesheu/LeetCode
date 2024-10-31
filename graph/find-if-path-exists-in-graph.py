5fb3e63e55a720634c951424afff70576d9232fd
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-10-31 14:08:39 -0700

    LC-find-if-path-exists-in-graph [Runtime: N/A; Memory: N/A]

---
+++ graph/find-if-path-exists-in-graph.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # undirected acyclic graph
+        # preprocess array of edges with hashmap to get neighbors easily
+        # dfs -> find connected component 
+        graph = defaultdict(list)
+        for x,y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+        seen = set()
+        def dfs(graph, node):
+            for neighbor in graph[node]:
+                if neighbor == destination:
+                    return True 
+                elif neighbor not in seen:
+                    seen.add(neighbor)
+                    dfs(graph,neighbor)
+            return False
+        return dfs(graph, source)
+        
+
+
+
