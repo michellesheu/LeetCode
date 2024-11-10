44833686405a399e168c2166ed352b7229645ca3
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-11-09 17:56:29 -0800

    LC-sort-colors [Runtime: N/A; Memory: N/A]

--- sorting/sort-colors.py
+++ sorting/sort-colors.py
@@ -3,7 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
-
-        
-        
+        l = -1
+        r = len(nums)
+        for i, n in enumerate(nums):
+            if i >= r:
+                break
+            if n == 0:
+                l += 1
+                nums[l], nums[i] = nums[i], nums[l]
+            elif n == 2:
+                r -= 1
+                nums[r], nums[i] = nums[i], nums[r]
