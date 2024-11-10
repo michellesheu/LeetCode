959809c8270cccffa2521cfd5e64736c42c9ecdc
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-11-09 18:51:46 -0800

    LC-sort-colors [Runtime: 0 ms; Memory: 16.5 MB]

--- array/sort-colors.py
+++ array/sort-colors.py
@@ -3,33 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # l = 0
-        # r = len(nums) - 1
-        # i = 0
-        # while i < len(nums):
-        #     if nums[i] == 0:
-        #         nums[i], nums[l] = nums[l], nums[i]
-        #         l += 1
-        #     elif nums[i] == 2:
-        #         nums[i], nums[r] = nums[r], nums[i]
-        #         r -= 1
-        #     else:
-        #         l+=1
-        #         r-=1
-        #     i += 1
-        l = 0
-        r = len(nums)-1
         i = 0
-        while i <= r:
-            n = nums[i]
-            if n == 0:
-                nums[l], nums[i] = nums[i], nums[l]
+        l = -1
+        r = len(nums)
+        while i < r:
+            if nums[i] == 0:
                 l += 1
-            elif n == 2:
-                nums[r], nums[i] = nums[i], nums[r]
-                r -= 1
-            if n == 1 or l > i:
+                nums[i], nums[l] = nums[l], nums[i]
                 i += 1
-
-
+            elif nums[i] == 2:
+                r -= 1
+                nums[i], nums[r] = nums[r], nums[i]
+            else:
+                i += 1
+        
 
