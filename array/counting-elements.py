dc847c2698c86a6a1dd0df02a7b9800a694a756b
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-11-26 08:43:17 -0800

    LC-counting-elements [Runtime: 1 ms; Memory: 17.4 MB]

--- array/counting-elements.py
+++ array/counting-elements.py
@@ -2,7 +2,7 @@ class Solution:
     def countElements(self, arr: List[int]) -> int:
         no_dupes = set(arr)
         count = 0
-        for num in no_dupes:
+        for num in arr:
             if num + 1 in no_dupes:
                 count += 1
         return count
