6e88a790edb78b72fd413c268a12d8fb6e411b06
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-04 00:06:34 -0800

    LC-remove-duplicates-from-sorted-array [Runtime: 3 ms; Memory: 18.4 MB]

--- two-pointers/remove-duplicates-from-sorted-array.py
+++ two-pointers/remove-duplicates-from-sorted-array.py
@@ -1,15 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0  # Handle edge case of an empty array
-
-        # Initialize the slow pointer
-        i = 0
-
-        # Iterate through the array with the fast pointer
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:  # Found a unique element
-                i += 1             # Move the slow pointer
-                nums[i] = nums[j]  # Update the position with the unique element
-
-        return i + 1  # Length of the array with unique elements
+        uniq = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[uniq] = nums[r]
+                uniq += 1
+        return uniq
