2ea9664e28384ac8904f307cbe9bf321a1b516fb
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-09 16:04:03 -0800

    LC-best-time-to-buy-and-sell-stock-ii [Runtime: 0 ms; Memory: 18.3 MB]

--- greedy/best-time-to-buy-and-sell-stock-ii.py
+++ greedy/best-time-to-buy-and-sell-stock-ii.py
@@ -1,17 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Iterate through the prices array from the second element
-        # Compare each price with the previous day's price
-        # If the current price is higher, add the difference to the total profit
-        # This approach ensures you capture every possible profitable transaction
-        if len(prices) == 1:
-            return 0
-        total = 0
-        profit = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                profit = prices[i] - prices[i-1]
-                total += profit
-        return total 
-
-
+        i = 0
+        valley = prices[0]
+        peak = prices[0]
+        maxprofit = 0
+        while i < len(prices) - 1:
+            while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
+                i += 1
+            valley = prices[i]
+            while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
+                i += 1
+            peak = prices[i]
+            maxprofit += peak - valley
+        return maxprofit
