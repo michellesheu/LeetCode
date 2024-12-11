227f14802759211e49f472ddd3f265fc2d1e1ff6
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-10 20:43:14 -0800

    LC-jump-game [Runtime: 9 ms; Memory: 18.1 MB]

--- dynamic-programming/jump-game.py
+++ dynamic-programming/jump-game.py
@@ -1,19 +1,7 @@
 class Solution:
     def canJump(self, nums: list[int]) -> bool:
-        # Initialize the maximum reachable index
-        reachable = 0
-        
-        # Traverse each index
-        for i in range(len(nums)):
-            # If the current index is beyond the maximum reachable, return False
-            if i > reachable:
-                return False
-            
-            # Update the maximum reachable index
-            reachable = max(reachable, i + nums[i])
-            
-            # Early exit if we already reached the last index
-            if reachable >= len(nums) - 1:
-                return True
-        
-        return False
+        goal = len(nums) - 1
+        for i in range(len(nums)-2, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+        return goal == 0
