d8d8f4312743f6371e5d9c968ea7afcc5fabf6fa
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-12 16:41:41 -0800

    LC-two-sum [Runtime: 3 ms; Memory: 18.2 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -2,9 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         char_ind = {}
         for i in range(len(nums)):
-            char_ind[nums[i]] = i
-        print(char_ind)
-        for i in range(len(nums)):
             pair = target - nums[i]
-            if pair in char_ind and i != char_ind[pair]:
+            if pair in char_ind:
                 return [i, char_ind[pair]]
+            char_ind[nums[i]] = i 
+            
