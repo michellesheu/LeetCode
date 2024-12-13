eb6a2c1c484b94f3407c0a9fe04142811be6ca91
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-12 21:30:18 -0800

    LC-top-k-frequent-elements [Runtime: 7 ms; Memory: 24.5 MB]

--- quickselect/top-k-frequent-elements.py
+++ quickselect/top-k-frequent-elements.py
@@ -1,12 +1,14 @@
-import heapq 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_freq = Counter(nums)
-        print(num_freq)
-        min_heap = []
-        for num, freq in num_freq.items():
-            heapq.heappush(min_heap, (freq, num))
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
-        result = [num for _, num in min_heap]
-        return result
+        # index is frequency val is list of nums
+        bucket = [[] for _ in range(len(nums)+1)]
+        count = Counter(nums).items()
+        print(count)
+        for num, freq in count:
+            bucket[freq].append(num)
+        flat_list = [item for sublist in bucket for item in sublist]
+        print(bucket)
+        print(bucket[::-1])
+        return flat_list[::-1][:k]
+
+            
