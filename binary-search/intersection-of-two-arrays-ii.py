e69fc9f3c0e48e07dbc6c007d93f64e8d8751a3f
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-14 21:40:41 -0800

    LC-intersection-of-two-arrays-ii [Runtime: 1 ms; Memory: 17.7 MB]

--- binary-search/intersection-of-two-arrays-ii.py
+++ binary-search/intersection-of-two-arrays-ii.py
@@ -1,7 +1,6 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         num1_freq = Counter(nums1)
-        num2_freq = Counter(nums2)
         ans = []
         for n in nums2:
             if n in num1_freq:
