283cac3025489be046b60fba1846b716434eb8bf
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-15 15:54:17 -0800

    LC-minimum-value-to-get-positive-step-by-step-sum [Runtime: 0 ms; Memory: 17.3 MB]

--- prefix-sum/minimum-value-to-get-positive-step-by-step-sum.py
+++ prefix-sum/minimum-value-to-get-positive-step-by-step-sum.py
@@ -1,11 +1,8 @@
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
-        current_sum = 0
         min_prefix_sum = 0
-        
-        for num in nums:
-            current_sum += num
-            min_prefix_sum = min(min_prefix_sum, current_sum)
-        
-        # Minimum starting value to ensure all sums are positive
+        prefix_sum = 0
+        for n in nums:
+            prefix_sum += n
+            min_prefix_sum = min(prefix_sum, min_prefix_sum)
         return 1 - min_prefix_sum
