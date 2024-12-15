f1d46e77e240ccb9b86763395403a53ccc9cdb72
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-14 21:35:56 -0800

    LC-intersection-of-two-arrays-ii [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/intersection-of-two-arrays-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        num1_freq = Counter(nums1)
+        num2_freq = Counter(nums2)
+        ans = []
+        if len(nums1) > len(nums2):
+            for n in nums2:
+                if n in num1_freq:
+                    num1_freq[n] -= 1
+                    ans.append(n)
+                    if num1_freq[n] == 0:
+                        del num1_freq[n]
+        else:
+            for n in nums1:
+                print(num1_freq)
+                print(n, num1_freq[n])
+                print(num2_freq)
+                if n in num2_freq:
+                    num2_freq[n] -= 1
+                    ans.append(n)
+                    if num2_freq[n] == 0:
+                        del num2_freq[n]
+        return ans 
+
+
