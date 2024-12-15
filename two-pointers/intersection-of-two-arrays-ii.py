bf75fd0688c58a12f4793fade75ad8e2a5f5d583
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-14 21:39:38 -0800

    LC-intersection-of-two-arrays-ii [Runtime: 3 ms; Memory: 17.5 MB]

--- two-pointers/intersection-of-two-arrays-ii.py
+++ two-pointers/intersection-of-two-arrays-ii.py
@@ -3,20 +3,12 @@ class Solution:
         num1_freq = Counter(nums1)
         num2_freq = Counter(nums2)
         ans = []
-        if len(nums1) > len(nums2):
-            for n in nums2:
-                if n in num1_freq:
-                    num1_freq[n] -= 1
-                    ans.append(n)
-                    if num1_freq[n] == 0:
-                        del num1_freq[n]
-        else:
-            for n in nums1:
-                if n in num2_freq:
-                    num2_freq[n] -= 1
-                    ans.append(n)
-                    if num2_freq[n] == 0:
-                        del num2_freq[n]
+        for n in nums2:
+            if n in num1_freq:
+                num1_freq[n] -= 1
+                ans.append(n)
+                if num1_freq[n] == 0:
+                    del num1_freq[n]
         return ans 
 
 
