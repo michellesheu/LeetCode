b777cfcf5d13f0b9126dd2e3ce7a09e74ad7935f
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-14 23:09:48 -0800

    LC-plus-one [Runtime: 0 ms; Memory: 17.4 MB]

--- array/plus-one.py
+++ array/plus-one.py
@@ -1,20 +1,17 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        all_nines = set(digits)
-        if len(all_nines) == 1 and 9 in all_nines:
-            for i in range(len(digits)):
-                digits[i] = 0
-            digits.insert(0, 1)
-            return digits
+        carry = 1
         for i in range(len(digits)-1, -1, -1):
-            if digits[i] == 9:
-                digits[i] = 0
-                digits[i-1] += 1
-            else:
-                digits[i] += 1
+            if digits[i] != 9:
+                digits[i] += carry
                 return digits
-                
+            else:
+                digits[i] = 0
+                carry = 1
+        if carry:
+            digits.insert(0,1)
+        return digits            
+
         # 9  1001
         # 10 1010
-        return list(map(lambda s: int(s), list(str(int("".join(map(lambda s: str(s), digits)))+1))))
         
