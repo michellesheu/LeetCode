bedc26398416f8a562f02a82a0ccbf2c944dbfc6
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-16 10:26:00 -0800

    LC-valid-palindrome [Runtime: 7 ms; Memory: 17.6 MB]

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -1,7 +1,5 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        print(s)
         l = 0
         r = len(s) - 1
         while l < r:
@@ -9,7 +7,7 @@ class Solution:
                 l += 1
             while l < r and not s[r].isalnum():
                 r -= 1
-            if s[l] != s[r]:
+            if s[l].lower() != s[r].lower():
                 return False
             l += 1
             r -= 1
