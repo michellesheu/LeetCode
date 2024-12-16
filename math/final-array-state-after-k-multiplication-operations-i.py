7bb751ba1b498ac3359993437b95273c875523e0
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-16 08:51:07 -0800

    LC-final-array-state-after-k-multiplication-operations-i [Runtime: 0 ms; Memory: 17.4 MB]

---
+++ math/final-array-state-after-k-multiplication-operations-i.py
@@ -0,0 +1,11 @@
+import heapq 
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        n = len(nums)
+        for i in range(k):
+            min_index = 0
+            for i in range(n):
+                if nums[i] < nums[min_index]:
+                    min_index = i
+            nums[min_index] *= multiplier
+        return nums
