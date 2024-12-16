d4fd248888557113d44727cf871bc2ad9ee498d0
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-16 09:01:56 -0800

    LC-final-array-state-after-k-multiplication-operations-i [Runtime: 3 ms; Memory: 17.3 MB]

--- array/final-array-state-after-k-multiplication-operations-i.py
+++ array/final-array-state-after-k-multiplication-operations-i.py
@@ -1,11 +1,24 @@
-import heapq 
+import heapq
+
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
         n = len(nums)
-        for i in range(k):
-            min_index = 0
-            for i in range(n):
-                if nums[i] < nums[min_index]:
-                    min_index = i
-            nums[min_index] *= multiplier
+        
+        # Initialize the min-heap with (value, index)
+        num_ind = [(nums[i], i) for i in range(n)]
+        heapq.heapify(num_ind)
+        
+        # Perform k updates
+        for _ in range(k):
+            min_val, i = heapq.heappop(num_ind)
+            # Multiply the smallest element
+            min_val *= multiplier
+            
+            # Update the original array
+            nums[i] = min_val
+            
+            # Push the updated value back into the heap
+            heapq.heappush(num_ind, (min_val, i))
+        
         return nums
+
