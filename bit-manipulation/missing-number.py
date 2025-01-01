59a7fceeff88e490409be1db95086cff6b5745e8
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-01-01 14:20:34 -0800

    LC-missing-number [Runtime: 8 ms; Memory: 18.8 MB]

--- bit-manipulation/missing-number.py
+++ bit-manipulation/missing-number.py
@@ -1,10 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        missing = 0
         n = len(nums)
-        missing_num = n
-        for i in range(n):
-            missing_num ^=i
-            missing_num ^= nums[i]
-
-        return missing_num
-        
+        for i in range(n+1):
+            missing ^= i
+        for n in nums:
+            missing ^= n
+        return missing
