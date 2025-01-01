ea5bc0f6d4321e1f45fa3f104e99d79da77ba3a5
michellesheu <50680760+michellesheu@users.noreply.github.com>
2024-12-31 21:26:53 -0800

    LC-make-array-zero-by-subtracting-equal-amounts [Runtime: 0 ms; Memory: 17.7 MB]

--- sorting/make-array-zero-by-subtracting-equal-amounts.py
+++ sorting/make-array-zero-by-subtracting-equal-amounts.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         nums = set(nums)
-        nums.remove(0)
+        if 0 in nums:
+            nums.remove(0)
         return len(nums)
