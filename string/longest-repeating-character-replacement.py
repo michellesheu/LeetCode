e6f19e9cb79c73e990fe119f933c67a095d47028
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-01-02 22:26:14 -0800

    LC-longest-repeating-character-replacement [Runtime: 123 ms; Memory: 18.2 MB]

---
+++ string/longest-repeating-character-replacement.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        longest = 0
+        l = 0
+        freq = [0] * 26
+        most_freq = 0 
+        for r in range(len(s)):
+            freq[ord(s[r]) - ord('A')] += 1
+            most_freq = max(most_freq, freq[ord(s[r]) - ord('A')])
+            
+            # Adjust window size if replacements exceed `k`
+            while (r - l + 1) - most_freq > k:
+                freq[ord(s[l]) - ord('A')] -= 1
+                l += 1
+            
+            # Update the longest valid window size
+            longest = max(longest, r - l + 1)
+        
+        return longest
