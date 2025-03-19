fdc52fce3fc8f1b81304c76dbd29b50f9d8d806c
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-03-18 20:45:38 -0400

    LC-two-sum [Runtime: 0 ms; Memory: 18.9 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,9 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        char_ind = {}
+        # key: num val: ind
+        # match = target - num and ind != curr 
+        # hashmap for o(1) lookup o(n) space
+        n_ind = {}
         for i in range(len(nums)):
-            pair = target - nums[i]
-            if pair in char_ind:
-                return [i, char_ind[pair]]
-            char_ind[nums[i]] = i 
-            
+            match = target - nums[i]
+            curr = nums[i]
+            if match in n_ind:
+                return [i, n_ind[match]]
+            n_ind[curr] = i
+        return [-1,-1]
