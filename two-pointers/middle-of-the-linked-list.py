63857234dd4cf258bceb6914540968086bf1fbfc
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-05-11 15:13:24 -0400

    LC-middle-of-the-linked-list [Runtime: 0 ms; Memory: 17.6 MB]

--- two-pointers/middle-of-the-linked-list.py
+++ two-pointers/middle-of-the-linked-list.py
@@ -5,9 +5,14 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = fast = head
+        # fast and slow pointer at head
+        # move fast 2 ahead of slow
+        # check if fast.next and fast are null to avoid access null error if fast is at final node
+        # slow will be at middle of list
+        fast = head
+        slow = head
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
+            slow = slow.next
         return slow
         
