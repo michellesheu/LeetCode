b741428077d31c74bb1027e2e864717bf3fcf465
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-07-06 12:47:41 -0700

    LC-palindrome-linked-list [Runtime: 48 ms; Memory: 34.8 MB]

--- two-pointers/palindrome-linked-list.py
+++ two-pointers/palindrome-linked-list.py
@@ -1,40 +1,13 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if not head or not head.next:
-            return True
-
-        # 1. Find the middle of the linked list
-        slow = head
-        fast = head
+    def isPalindrome(self, head: ListNode) -> bool:
+        slow, fast, prev = head, head, None
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-
-        # 2. Reverse the second half
-        def reverse_ll(node):
-            prev = None
-            curr = node
-            while curr:
-                next_temp = curr.next
-                curr.next = prev
-                prev = curr
-                curr = next_temp
-            return prev
-        
-        second_half_reversed = reverse_ll(slow)
-
-        # 3. Compare the first and reversed second halves
-        first_half = head
-        while second_half_reversed:
-            if first_half.val != second_half_reversed.val:
-                return False
-            first_half = first_half.next
-            second_half_reversed = second_half_reversed.next
-            
+            slow, fast = slow.next, fast.next.next
+        prev, slow, prev.next = slow, slow.next, None
+        while slow:
+            slow.next, prev, slow = prev, slow, slow.next
+        fast, slow = head, prev
+        while slow:
+            if fast.val != slow.val: return False
+            fast, slow = fast.next, slow.next
         return True
