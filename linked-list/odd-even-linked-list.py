78ab0d853c444a180b4bf0d7bdd21d55eb1ef35c
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-07-14 19:07:32 -0700

    LC-odd-even-linked-list [Runtime: N/A; Memory: N/A]

---
+++ linked-list/odd-even-linked-list.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        odd_tail = head
+        even_tail = head.next
+        even_head = even_tail
+        # even will be at end of list
+        while even_tail and even_tail.next:
+            odd_tail.next = odd_tail.next.next
+            even_tail.next = even_tail.next.next
+            odd_tail = odd_tail.next
+            even_tail = even_tail.next
+        odd_tail.next = even_head
+        return head
