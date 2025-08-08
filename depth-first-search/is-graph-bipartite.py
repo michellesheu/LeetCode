4e32800a3b3315b060e26c551c94f9b06fbc62f8
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-08-07 17:57:43 -0700

    LC-is-graph-bipartite [Runtime: 0 ms; Memory: 18.6 MB]

---
+++ depth-first-search/is-graph-bipartite.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        color = [-1] * n  # -1 means unvisited
+
+        def dfs(node, c):
+            color[node] = c
+            for neighbor in graph[node]:
+                if color[neighbor] == -1:
+                    if not dfs(neighbor, 1 - c):
+                        return False
+                elif color[neighbor] == c:
+                    return False
+            return True
+
+        for i in range(n):
+            if color[i] == -1:
+                if not dfs(i, 0):  # Start coloring with 0
+                    return False
+        return True
