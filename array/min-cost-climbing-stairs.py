231846f1df9c5dd2f7afb8abf62f2ea65f4a0d48
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-09-16 16:21:11 -0700

    LC-min-cost-climbing-stairs [Runtime: 7 ms; Memory: 20.2 MB]

---
+++ array/min-cost-climbing-stairs.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # top down 
+        # 1. define a recursive function dp that returns the min cost up to ith index of cost array
+        # 2. base case is len(cost) == 1, pay cost[0]
+        # 2. len(cost) == 2, return min(cost[0], cost[1])
+        # 3. top floor is the index out of bounds of cost array
+        # 4. recurrence relation is min(dp(i-1)+cost[i], dp(i-2)+cost[i])
+        # 1.
+        def dp(i):
+            # 2.
+            if i <= 1:
+                return 0
+            if i in memo:
+                return memo[i]
+            memo[i] = min(dp(i-1)+cost[i-1], dp(i-2)+cost[i-2])
+            return memo[i]
+        n = len(cost)
+        memo = {}
+        return dp(n)
+            
+            
