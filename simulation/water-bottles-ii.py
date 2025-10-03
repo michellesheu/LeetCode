0ad5bd8961e8ff306d02f178beb4c5f5cb6d9c42
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-10-02 20:21:52 -0700

    LC-water-bottles-ii [Runtime: 46 ms; Memory: 17.9 MB]

--- simulation/water-bottles-ii.py
+++ simulation/water-bottles-ii.py
@@ -1,37 +1,10 @@
 class Solution:
     def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
-        # always gonna drink full bottles initially to get empty bottles to exchange
-        # set empty bottles to full bottles drunk
-        # set full to 0 bc drunk
-        # check while num_empty >= num_exchange or num_full > 0
-        # if true: exchange numExchange for one full bottle num_empty- num_exchange num_exchange ++ full_bottle ++
-        # if false: drink the full bottles add to max_bottles add to empty bottles
-
-        # initial drinking
         max_bottles = numBottles
-        num_empty = numBottles
-        num_full = 0
-        while num_empty >= numExchange:
-            print(f"{num_empty=}")
-            print(f"{numExchange=}")
-            print("---------------")
-            num_empty -= numExchange
+        empty = numBottles
+        while empty >= numExchange:
+            empty -= numExchange
             numExchange += 1
-            num_full += 1
-            print(f"{num_full=}")
-            print(f"{num_empty=}")
-            print(f"{numExchange=}")
-            print("---------------")
-            print("---------------")
-            # need to drink to exchange
-            if num_full > 0 and num_empty < numExchange:
-                max_bottles += num_full
-                num_empty += num_full
-                num_full = 0
-                print("we drinking")
-                print(f"{num_full=}")
-                print(f"{num_empty=}")
-                print(f"{numExchange=}")
-                print(f"{max_bottles=}")
-            
+            empty += 1
+            max_bottles += 1
         return max_bottles
