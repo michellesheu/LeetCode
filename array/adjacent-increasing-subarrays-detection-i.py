11427a8e643dbb3b984cae666fa9239c13d4a971
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-10-13 22:53:46 -0700

    LC-adjacent-increasing-subarrays-detection-i [Runtime: N/A; Memory: N/A]

---
+++ array/adjacent-increasing-subarrays-detection-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
+        # input: nums, size
+        # output: return true if valid
+        curr = 0
+        n = len(nums)
+        longest_increasing = 1
+        first_valid = False
+        for i in range(len(nums)-1):
+            print(f"{nums[i]=} {nums[i+1]=}")
+            print(f"{longest_increasing=}")
+            if nums[i] < nums[i+1]:
+                longest_increasing += 1
+            if longest_increasing >= 2*k:
+                return True
+            elif nums[i] >= nums[i+1]:
+                if longest_increasing >= k:
+                    first_valid = True
+                    longest_increasing = 1
+                else:
+                    first_valid = False
+            if first_valid and longest_increasing >= k:
+                return True
+        return False
+            
+
