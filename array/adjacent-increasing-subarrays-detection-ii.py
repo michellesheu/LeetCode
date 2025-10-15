b18b7e711435d2ae6ea07b0b5d74bd8d0e1b037a
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-10-15 00:44:47 -0700

    LC-adjacent-increasing-subarrays-detection-ii [Runtime: N/A; Memory: N/A]

--- array/adjacent-increasing-subarrays-detection-ii.py
+++ array/adjacent-increasing-subarrays-detection-ii.py
@@ -2,19 +2,19 @@ class Solution:
     def maxIncreasingSubarrays(self, nums: List[int]) -> int:
         longest_increasing = 1
         prev_increasing  = 0
-        k = 0
+        k = float("-inf")
         for i in range(len(nums)-1):
             print(f"before: {longest_increasing=}")
             if nums[i] < nums[i+1]:
                 longest_increasing += 1
             elif nums[i] >= nums[i+1]:
-                print(f"after: {longest_increasing=}")
+                print(f"break after: {longest_increasing=}")
                 k = max(min(longest_increasing, prev_increasing), longest_increasing//2, k)
                 print(f"{k=}")
                 prev_increasing = longest_increasing
                 print(f"{prev_increasing=}")
                 longest_increasing = 1
-
+            print(f"outside after: {longest_increasing=}")
 
         return k
                
