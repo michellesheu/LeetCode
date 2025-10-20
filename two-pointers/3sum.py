d0ecb3914bd442f9830ab4d1ca91a6a7babb08dd
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-10-19 18:44:35 -0700

    LC-3sum [Runtime: 767 ms; Memory: 20.9 MB]

--- two-pointers/3sum.py
+++ two-pointers/3sum.py
@@ -4,15 +4,20 @@ class Solution:
         n = len(nums)
         res = []
         for i in range(n-2):
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
             curr = nums[i]
             j = i + 1
             k = n - 1
             while j < k:
                 if nums[j] + nums[k] == -curr:
-                    if [curr, nums[j], nums[k]] not in res:
-                        res.append([curr, nums[j], nums[k]])
+                    res.append([curr, nums[j], nums[k]])
                     j += 1
                     k -= 1
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
+                    while k > j and nums[k] == nums[k+1]:
+                        k -= 1
                 elif nums[j] + nums[k] < -curr:
                     j += 1
                 else:
