202c7e6503ed6ab4708d2a652c866b33e88e1359
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-12-18 23:31:06 -0800

    LC-valid-anagram [Runtime: 53 ms; Memory: 17.6 MB]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -4,17 +4,18 @@ class Solution:
         # output: return true if anagram (every char in s is in t/same frequency, same length)
         # make a dict of t's char freqs and use to keep track of frequencies in s that are in t and subtract if in t if freq < 0 return False if char not in t return False
         # t_chars {c:1,a:1,r:1}
-        # O(s) time bc look through every char in s and O(1) lookup in dict
-        # O(t) space to make dict of t's char keys
+        # O(n), n is length of both strings time bc look through every char in s and O(1) lookup in dict
+        # O(k) = O(1) space to make dict of t's lowercase English char keys (26)
         if len(s) != len(t):
             return False
-        t_chars = {}
+        t_chars = [0]*26
         for char in t:
-            t_chars[char] = t_chars.get(char, 0) + 1
+            print(f"{char}= {ord(char)}= {ord(char)-ord('a')}=")
+            t_chars[ord(char)-ord('a')] += 1
         for char in s:
-            if char in t_chars:
-                t_chars[char] -= 1
-                if t_chars[char] < 0:
+            if t_chars[ord(char)-ord('a')] > 0:
+                t_chars[ord(char)-ord('a')] -= 1
+                if t_chars[ord(char)-ord('a')] < 0:
                     return False
             else:
                 return False
