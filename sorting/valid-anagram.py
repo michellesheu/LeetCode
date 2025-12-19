f86a06e65e1367f0568d3694efa37d59b98550c6
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-12-18 23:13:33 -0800

    LC-valid-anagram [Runtime: 15 ms; Memory: 17.6 MB]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,16 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # build a hashmap for s to keep track of char freq
-        # iterate through t 
-        # if t char in s hashmap delete t from s hashmap
-        # if hashmap value < 0 then return false
-        # if hashmap values = 0 for all chars then true 
-        s_count = Counter(s)
-        for ch in t:
-            if ch in s_count:
-                s_count[ch] -= 1
-                if s_count[ch] < 0:
+        # input: s: string t: string lowercase english
+        # output: return true if anagram (every char in s is in t/same frequency, same length)
+        # make a dict of t's char freqs and use to keep track of frequencies in s that are in t and subtract if in t if freq < 0 return False if char not in t return False
+        # t_chars {c:1,a:1,r:1}
+        # O(s) time bc look through every char in s and O(1) lookup in dict
+        # O(t) space to make dict of t's char keys
+        if len(s) != len(t):
+            return False
+        t_chars = {}
+        for char in t:
+            t_chars[char] = t_chars.get(char, 0) + 1
+        for char in s:
+            if char in t_chars:
+                t_chars[char] -= 1
+                if t_chars[char] < 0:
                     return False
             else:
                 return False
-        return all(value == 0 for value in s_count.values())
+        return True
+            
