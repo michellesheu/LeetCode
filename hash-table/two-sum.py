2d161b90e29848851c7985b87a3313a434782dee
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-12-22 11:57:52 -0800

    LC-two-sum [Runtime: 4 ms; Memory: 18.6 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,14 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # input: list of nums
-        # output: [i,j] i!=j nums[i]+nums[j]=target, diff indices of two nums that sum to target
-        # make a hashmap of val:i bc we want to look for values in hashmap and keep track of their indices, complement = target - curr_val, if curr_val_i!= complement_val_j and complement in hashmap then return [curr_val_i, complement_val_j]
-        val_ind = {}
-        for i in range(len(nums)):
-            val_ind[nums[i]] = i
+        # create hashmap entries as we iterate thru nums and check if complement is in hashmap
+        num_ind = {}
         for i in range(len(nums)):
             complement = target - nums[i]
-            print(f"{complement=}")
-            if complement in val_ind and i != val_ind[complement]:
-                return [i,val_ind[complement]]
-        return [-1,-1]
+            if complement in num_ind:
+                return [num_ind[complement], i]
+            num_ind[nums[i]] = i
+         
