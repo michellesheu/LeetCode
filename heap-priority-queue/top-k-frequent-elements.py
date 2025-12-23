df90e7f1823293f2e7c7f7b156e930f2ca4119c1
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-12-23 13:22:23 -0800

    LC-top-k-frequent-elements [Runtime: 11 ms; Memory: 23.1 MB]

--- heap-priority-queue/top-k-frequent-elements.py
+++ heap-priority-queue/top-k-frequent-elements.py
@@ -1,6 +1,31 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = Counter(nums)
-        # Sort the keys of the dictionary based on their frequency values
-        # Then take the first k elements
-        return sorted(freq, key=freq.get, reverse=True)[:k]
+        # bucket sort O(n) time complexity 
+        # store nested list of size len(nums) + 1 
+        # length of list is n + 1 bc at most n frequent elements, 0-n
+        # each index represents 0-n frequency, values stored in nested list are the elements of that frequency
+        # iterate through freq_map to create bucket arr
+        # iterate thru bucket_elems reversed from most freq to least freq, add elem in that bucket to output list until len of list is k then return output
+        output = []
+        n = len(nums)
+        bucket_elems = [[] for _ in range(n + 1)]
+        # print(f"{bucket_elems=}")
+        freq_map = Counter(nums)
+        # print(f"{freq_map=}")
+        for num,count in freq_map.items():
+            # print(f"---{bucket_elems=}---")
+            # print(f"{count=} {bucket_elems[count]=}")
+            bucket_elems[count].append(num)
+            # print(f"{count=} {bucket_elems[count]=}")
+            # print(f"---{bucket_elems=}---")
+        print(f"{bucket_elems=}")
+        for bucket in reversed(bucket_elems):
+            for elem in bucket:
+                # print(f'{elem=}')
+                output.append(elem)
+                # print(f'{output=}')
+                # print(f'{len(output)=}')
+                if len(output) == k:
+                    return output
+
+
