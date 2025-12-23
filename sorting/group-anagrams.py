0de402cdf817f86e25a22a978239bd428460c0ad
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-12-23 12:16:14 -0800

    LC-group-anagrams [Runtime: 23 ms; Memory: 21.8 MB]

--- sorting/group-anagrams.py
+++ sorting/group-anagrams.py
@@ -1,14 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # create a dict with uniq key of sorted str anagram and value as list of str anagrams
-        # iterate thru str in strs sort the str and add str to dict if in dict else add new entry
-        # return list of dict values
-        anagrams = []
-        ana_strs = {}
+        # bc input strs is lowercase letters make tuple by converting arr of len 26 as a key signature of anagrams
+        # char maps to index ex. 'a' is index 0, determine freq of each char and store in corresponding index
+        # iterate through each char in str in strs and make key signature
+        # return the values in dict
+
+        freq_map = defaultdict(list)
         for str in strs:
-            ana = ''.join(sorted(str))
-            if ana in ana_strs:
-                ana_strs[ana].append(str)
-            else:
-                ana_strs[ana] = [str]
-        return list(ana_strs.values())
+            possible_chars = [0] * 26
+            for ch in str:
+                ch_index = ord(ch)-ord('a')
+                possible_chars[ch_index] += 1
+            freq_map[tuple(possible_chars)].append(str)
+        return list(freq_map.values())
+
