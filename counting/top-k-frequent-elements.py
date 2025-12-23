7e0bef4e0580fbdd2d4e3aa1ac6659c7c4b7d081
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-12-22 22:28:02 -0800

    LC-top-k-frequent-elements [Runtime: 6 ms; Memory: 20.6 MB]

--- counting/top-k-frequent-elements.py
+++ counting/top-k-frequent-elements.py
@@ -1,14 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # index is frequency val is list of nums
-        bucket = [[] for _ in range(len(nums)+1)]
-        count = Counter(nums).items()
-        print(count)
-        for num, freq in count:
-            bucket[freq].append(num)
-        flat_list = [item for sublist in bucket for item in sublist]
-        print(bucket)
-        print(bucket[::-1])
-        return flat_list[::-1][:k]
-
-            
+        freq = Counter(nums)
+        # Sort the keys of the dictionary based on their frequency values
+        # Then take the first k elements
+        return sorted(freq, key=freq.get, reverse=True)[:k]
