1517f975b0e2c237f8a7fcbc26d53b4422981d8e
michellesheu <50680760+michellesheu@users.noreply.github.com>
2025-12-22 16:03:25 -0800

    LC-group-anagrams [Runtime: 7 ms; Memory: 19.9 MB]

--- sorting/group-anagrams.py
+++ sorting/group-anagrams.py
@@ -1,7 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        freq_strs = defaultdict(list)
-        for s in strs:
-            freq_strs[tuple(sorted(s))].append(s)
-        print(freq_strs)
-        return list(freq_strs.values())
+        # create a dict with uniq key of sorted str anagram and value as list of str anagrams
+        # iterate thru str in strs sort the str and add str to dict if in dict else add new entry
+        # return list of dict values
+        anagrams = []
+        ana_strs = {}
+        for str in strs:
+            ana = ''.join(sorted(str))
+            if ana in ana_strs:
+                ana_strs[ana].append(str)
+            else:
+                ana_strs[ana] = [str]
+        return list(ana_strs.values())
