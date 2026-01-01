cb0607347f5fa0c975423522d7c6e0912061f707
michellesheu <50680760+michellesheu@users.noreply.github.com>
2026-01-01 14:50:00 -0800

    LC-valid-sudoku [Runtime: 3 ms; Memory: 17.3 MB]

--- hash-table/valid-sudoku.py
+++ hash-table/valid-sudoku.py
@@ -1,18 +1,28 @@
 from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row_digits = defaultdict(set)
-        col_digits = defaultdict(set)
-        box_digits = defaultdict(set)
-        m = len(board)
-        n = len(board[0])
-        for row in range(m):
-            for col in range(n):
-                if board[row][col] == '.':
+        # Initialize your three defaultdict(set) objects.
+        # Loop through $r$ from $0$ to $8$
+        # :Loop through $c$ from $0$ to $8$:
+        # Get the value $v$ at board[r][c].
+        # If $v$ is ".", skip.
+        # Check the three sets using the expression above.
+        # Return False if a duplicate is found.
+        # Add $v$ to rows[r], cols[c], and blocks[(r // 3, c // 3)].
+        # Return True at the very end.
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        blocks = defaultdict(set)
+        row_len = len(board)
+        col_len = len(board[0])
+        for r in range(row_len):
+            for c in range(col_len):
+                val = board[r][c]
+                if val == ".":
                     continue
-                if (board[row][col] in row_digits[row] or board[row][col] in col_digits[col] or board[row][col] in box_digits[(row//3, col//3)]):
+                elif val in rows[r] or val in cols[c] or val in blocks[(r//3,c//3)]:
                     return False
-                row_digits[row].add(board[row][col])
-                col_digits[col].add(board[row][col])
-                box_digits[(row//3, col//3)].add(board[row][col])
+                rows[r].add(val)
+                cols[c].add(val)
+                blocks[(r//3,c//3)].add(val)
         return True
