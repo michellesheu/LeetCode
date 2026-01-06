b8f8ffd37d46aba78c4109c9ccd269720fb4bc70
michellesheu <50680760+michellesheu@users.noreply.github.com>
2026-01-06 11:00:17 -0800

    LC-longest-consecutive-sequence [Runtime: 58 ms; Memory: 36.7 MB]

--- array/longest-consecutive-sequence.py
+++ array/longest-consecutive-sequence.py
@@ -1,12 +1,21 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums = set(nums)
+        # Store all numbers in a Hash Set for $O(1)$ lookups.
+        # Iterate through the set.
+        # Identify the start of a sequence by checking if $x-1$ is missing from the set.
+        # Count the streak for starters only by checking for $x+1, x+2, \dots$ until the chain breaks.
+        # Update the maximum length found so far.
+        uniq_nums = set(nums)
         max_len = 0
-        for n in nums:
-            print("n:", n)
-            if n-1 not in nums:
-                y = n+1
-                while y in nums:
-                    y += 1
-                max_len = max(max_len, y - n)
+        for num in uniq_nums:
+            # check if num is start of seq
+            if num - 1 not in uniq_nums:
+                cons_len = 1
+                # check if consecutive element in set
+                while num + 1 in uniq_nums:
+                    cons_len += 1
+                    num += 1
+                max_len = max(max_len, cons_len)
         return max_len
+                
+
