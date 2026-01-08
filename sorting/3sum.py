4628312e30d1b7b939019eac7ae58ad24ffa6073
michellesheu <50680760+michellesheu@users.noreply.github.com>
2026-01-08 13:02:24 -0800

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.py
+++ sorting/3sum.py
@@ -1,25 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # return list of lists of unique indexed triplet == 0 
+        # sort array first to handle duplicate numbers in order
+        # fix a number, use two pointers to get the additive inverse and update ptrs if sum too big or small, if sum is equal then add to output and move pointers toward each other, skip duplicate numbers in array and move pointers towards each other, end loop at len(nums)-1
+        # 
+        # [-1, 0, 1, 2, -1]
+        # [-1, -1, 0, 1, 2]
+        #
         nums.sort()
-        n = len(nums)
-        res = []
-        for i in range(n-2):
+        output = []
+        for i in range(len(nums)-1):
             if i != 0 and nums[i] == nums[i-1]:
                 continue
-            curr = nums[i]
-            j = i + 1
-            k = n - 1
-            while j < k:
-                if nums[j] + nums[k] == -curr:
-                    res.append([curr, nums[j], nums[k]])
-                    j += 1
-                    k -= 1
-                    while j < k and nums[j] == nums[j-1]:
-                        j += 1
-                    while k > j and nums[k] == nums[k+1]:
-                        k -= 1
-                elif nums[j] + nums[k] < -curr:
-                    j += 1
+            curr_target = -nums[i] # 1
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                if nums[left] + nums[right] == curr_target:
+                    output.append([nums[left], nums[right],nums[i]])
+                    left += 1
+                    right -= 1
+                elif nums[left] + nums[right] < curr_target:
+                    left += 1
                 else:
-                    k -= 1
-        return res
+                    right -= 1
+        return output
+
+
