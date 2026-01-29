9f61e4e52483736f706d0abd4ba00bcc03f53cf7
michellesheu <50680760+michellesheu@users.noreply.github.com>
2026-01-29 15:57:32 -0800

    LC-permutations [Runtime: 48 ms; Memory: 19.6 MB]

--- array/permutations.py
+++ array/permutations.py
@@ -1,14 +1,31 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(curr):
-            if len(curr) == len(nums):
-                ans.append(curr[:])
+        # // let curr represent the thing you are building
+        # // it could be an array or a combination of variables
+
+        # function backtrack(curr) {
+        #     if (base case) {
+        #         Increment or add to answer
+        #         return
+        #     }
+
+        #     for (iterate over input) {
+        #         Modify curr
+        #         backtrack(curr)
+        #         Undo whatever modification was done to curr
+        #     }
+        # }
+        permutations = []
+        def backtrack(curr_permutation):
+            if len(curr_permutation) == len(nums):
+                permutations.append(curr_permutation[:]) # copy curr_permutation?
                 return
             for num in nums:
-                if num not in curr:
-                    curr.append(num)
-                    backtrack(curr)
-                    curr.pop()
-        ans = []
+                if num in curr_permutation: # violates constraint of uniqueness
+                    continue
+                curr_permutation.append(num) # choose num
+                # print(f"{curr_permutation=}")
+                backtrack(curr_permutation) # curr_permutation modified closer to basecase, explore to successor after adding num
+                curr_permutation.pop() # unchoose num
         backtrack([])
-        return ans
+        return permutations
