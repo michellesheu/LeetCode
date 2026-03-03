65584df41667339c32384b7a42b05d9f96f7f491
michellesheu <50680760+michellesheu@users.noreply.github.com>
2026-03-02 22:09:41 -0800

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- stack/valid-parentheses.py
+++ stack/valid-parentheses.py
@@ -5,14 +5,20 @@ class Solution:
         # m: stack problem to keep track of last bracket popped is closing for ch in s check if stack is not empty and what the top of the stack is and see if it matches if not return false add opening brackets to stack, if stack is empty in the end return true else return false bc that means opening brackets still inside
         stack = []
         for parenthesis in s:
+            print(f'{parenthesis=}')
             if parenthesis == '(' or parenthesis == '[' or parenthesis == '{':
                 stack.append(parenthesis)
-            elif stack:
+                print(f'{stack=}')
+            elif not stack:
+                return False
+            else:
                 top = stack.pop()
+                print(f"{top=}")
                 if top == '(' and parenthesis != ')':
                     return False
                 elif top == '[' and parenthesis != ']':
                     return False
                 elif top == '{' and parenthesis != '}':
                     return False
+        print(stack)
         return True if not stack else False
