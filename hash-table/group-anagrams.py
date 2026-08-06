013858938209fdcf94c9ba179b3125ca773c4dde
michellesheu <50680760+michellesheu@users.noreply.github.com>
2026-08-05 21:40:29 -0700

    LC-group-anagrams [Runtime: N/A; Memory: N/A]

--- hash-table/group-anagrams.py
+++ hash-table/group-anagrams.py
@@ -1,16 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # bc input strs is lowercase letters make tuple by converting arr of len 26 as a key signature of anagrams
-        # char maps to index ex. 'a' is index 0, determine freq of each char and store in corresponding index
-        # iterate through each char in str in strs and make key signature
-        # return the values in dict
-
-        freq_map = defaultdict(list)
-        for str in strs:
-            possible_chars = [0] * 26
-            for ch in str:
-                ch_index = ord(ch)-ord('a')
-                possible_chars[ch_index] += 1
-            freq_map[tuple(possible_chars)].append(str)
-        return list(freq_map.values())
-
+        # input: strs a list of lowercase letter strings aka can use a 26 ch array
+        # output: a list of list of anagrams (same freq of chars aka same len)
+        # make hashmap to keep track of sortedchar_listofstr(sorted char is unique key) by checking if sortedchar in hashmap then add to list else add new key to hashmap
+        # "aet":["ate", "eat", "tea"], "ant":[...], "abt":[...]
+        # add hashmap values to output list
+        sorted_ana = {}
+        for s in strs:
+            key = "".join(sorted(s))
+            if key in sorted_ana:
+                sorted_ana[key].append(s)
+            else:
+                sorted_ana[key] = [s]
+        return list(sorted_ana.values())
