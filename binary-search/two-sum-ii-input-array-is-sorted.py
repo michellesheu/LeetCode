2d1c964338a1faf0d0b297701f9125f305c4c788
michellesheu <50680760+michellesheu@users.noreply.github.com>
2026-08-06 16:01:40 -0700

    LC-two-sum-ii-input-array-is-sorted [Runtime: N/A; Memory: N/A]

--- binary-search/two-sum-ii-input-array-is-sorted.py
+++ binary-search/two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,19 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # input: list of sorted nums in increasing order
+        # output: return list of indices+1 that sum to target
+        # use 2 ptrs for O(1) space constraint with left as lower and right as upper bound
+        # while left < right
+        # if num[left] + num[right] == target, return [left+1,right+1]
+        # elif sum is too big, right -= 1
+        # else (sum too small) left += 1
         left = 0
         right = len(numbers) - 1
         while left < right:
-            if numbers[left] + numbers[right] == target:
-                return [left + 1, right + 1]
-            elif numbers[left] + numbers[right] < target:
-                left += 1
-            else:
+            t_sum = numbers[left] + numbers[right]
+            if t_sum == target:
+                return [left+1,right+1]
+            elif t_sum > target:
                 right -= 1
-        
+            else:
+                left +=1
