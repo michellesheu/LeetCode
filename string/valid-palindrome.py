9dfcf11478f624f9501c4ab45078ba691dbe6793
michellesheu <50680760+michellesheu@users.noreply.github.com>
2026-08-06 15:49:36 -0700

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

--- string/valid-palindrome.py
+++ string/valid-palindrome.py
@@ -1,21 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # input: s is a string of alphanumeric(letters and numbers)and non-alphanumeric chars
+        # output: true if palindrome all lowercase and alphanumeric chars match back and forward else false 
+        # brute force: 
+        # 2 ptrs: convert all uppercase chs to lowercase and check if not alpha num move ptrs to alphanum ch, if alphanum and match backward to forward, decrement right ptr and increment left ptr, if not = then return false immediately, return true at end of for loop
         left = 0
         right = len(s) - 1
         while left < right:
-            # handle index out of bounds and move pointers to alnum chs
             while not s[left].isalnum():
-                print(f"{left=}")
                 left += 1
-                print(f"{left=}")
             while not s[right].isalnum():
-                print(f"{right=}")
                 right -= 1
-                print(f"{right=}")
-            # handle matching alnum and lowercase
-            if s[left].lower() == s[right].lower():
-                left += 1
-                right -= 1
-            else:
+            # now left and right are at alpha num chs
+            if s[left].lower() != s[right].lower():
                 return False
+            left += 1
+            right -= 1
         return True
+            
