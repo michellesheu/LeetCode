5b3a5fd5dc233a3ae7972cc1ceb27a901bcec231
michellesheu <50680760+michellesheu@users.noreply.github.com>
2026-08-07 16:07:48 -0700

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- hash-table/longest-substring-without-repeating-characters.py
+++ hash-table/longest-substring-without-repeating-characters.py
@@ -1,19 +1,24 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0 
-        uniq = set()
-        max_size = 1
-        if not s:
-            return 0
+        # input: s is a string
+        # output: length of longest nonempty contiguous unique characters substring 
+        # use sliding window two ptrs to keep track of left and right of substring window and use a set to keep track of seen characters 
+        # iterate thru s 
+        # while curr ch is in seen, remove left ch from set move left 
+        # add ch to set, update curr length= right-left+1 and update max len of valid window move right ptr
+        # return max len of window
+        max_len = 0
+        seen = set()
+        left = 0
         for right in range(len(s)):
-            print(right, s[right], uniq)
-            while s[right] in uniq:
-                uniq.remove(s[left])
+            #invalid window
+            while s[right] in seen:
+                seen.remove(s[left])
                 left += 1
-                print(right, left)
-            uniq.add(s[right])  
-            max_size = max(right-left+1, max_size)
-            print(max_size)
-        return max_size
+            # valid window
+            seen.add(s[right])
+            curr_len = right - left + 1
+            max_len = max(max_len, curr_len)
+        return max_len
 
 
